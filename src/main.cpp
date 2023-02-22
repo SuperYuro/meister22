1f055abb9ca9d434c3ca9b576b1852867c0859ff
SuperYuro <a.gpgtdmgp@gmail.com>
2023-02-22 19:21:59 +0900

    サーボ動いた

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+const unsigned long wait = 1000;
 
 const int onboard_led = 13;
+
+const int black_button = 6;
+const int white_button = 5;
+
+const int main_servo = 4;
+Servo servo;
+
+bool flag = true;
+
+int angle = 0;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Serial started");
+
     pinMode(onboard_led, OUTPUT);
+
+    pinMode(black_button, INPUT);
+    pinMode(white_button, INPUT);
+
+    servo.attach(main_servo);
 }
 
 void loop() {
-    digitalWrite(onboard_led, HIGH);
-    Serial.println("LED is on");
-    delay(200);
-    digitalWrite(onboard_led, LOW);
-    Serial.println("LED is off");
+    if (!digitalRead(white_button)) {
+        servo.write(180);
+        Serial.println("Now: 180");
+    }
+    if (!digitalRead(black_button)) {
+        servo.write(90);
+        Serial.println("Now: 90");
+    }
     delay(200);
 }
